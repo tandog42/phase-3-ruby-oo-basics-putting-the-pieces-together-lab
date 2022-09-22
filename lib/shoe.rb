b553a5c30499c98466ef8ab4ac77801b2b1daaf2
Tanner <troxs690@gmail.com>
2022-09-22 11:48:49 -0500

    d

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
-# Make your shoe class here!
+class Shoe
+attr_reader :brand
+attr_writer :condition
+attr_accessor :color, :size, :material,:condition
+
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+
+
+  def cobble
+    @condition = "new"
+   puts "Your shoe is as good as new!"
+  end
+
+
+
+
+end
