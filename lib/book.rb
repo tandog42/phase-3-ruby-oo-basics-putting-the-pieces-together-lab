3b86d5391f48671d0a4315ec7b651c0a2c7ae37b
Tanner <troxs690@gmail.com>
2022-09-22 11:39:05 -0500

    commit tree b4c9d4f4dd3f8cc5387c5c6dfaad2f6e8b0d96ea

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
 
 
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
+ready = Book.new("lilly")
+
+ready.page_count = 100
+ready.genre = "Horror"
+ready.turn_page
